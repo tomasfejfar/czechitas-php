01b88c83d6fd948240c3d6c934b1f54780a8cf6b
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-10 12:08:18 +0100

    A vypíšeme je cyklem

--- index.php
+++ index.php
@@ -16,6 +16,14 @@ $me = [
 ?>
 <h1><?= $greeting ?></h1>
 
+<h2>O mně</h2>
+<ul>
+    <?php foreach ($me as $characteristic) { ?>
+        <li><?= $characteristic ?></li>
+    <?php } ?>
+</ul>
+
+
 <h2>Záliby</h2>
 <ul>
     <?php foreach ($hobbies as $hobby) { ?>
