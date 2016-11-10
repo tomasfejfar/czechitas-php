340a9b7ebb6f2e81b4572cfa8049963c1dad8fd8
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-10 11:54:52 +0100

    Vypíšeme si záliby

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 $name = 'Tom';
 $greeting = 'Hello ' . $name;
+
+$hobby1 = 'trekking';
+$hobby2 = 'programování';
 ?>
 <h1><?= $greeting ?></h1>
+
+<h2>Záliby</h2>
+<ul>
+    <li><?= $hobby1 ?></li>
+    <li><?= $hobby2 ?></li>
+</ul>
