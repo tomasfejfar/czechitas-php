6c51694f1cbea3031701975376dc05e789a683a1
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 13:25:58 +0100

    Kód pro o-mne přesuneme do souboru

---
+++ controllers/o-mne.php
@@ -0,0 +1,24 @@
+<?php
+$hobbies = [
+    'trekking',
+    'programování',
+    'školení',
+    'procházky',
+];
+
+$me = [
+    'Jméno' => 'Tomáš',
+    'Příjmení' => 'Fejfar',
+];
+
+$friends = [
+    [
+        'name' => 'Lenka',
+        'surname' => 'Krafková',
+    ],
+    [
+        'name' => 'Tomáš',
+        'surname' => 'Kormaňák',
+    ],
+];
+$template = 'o-mne';
--- index.php
+++ index.php
@@ -3,28 +3,6 @@
 if (empty($_GET['page'])) {
     require 'controllers/hlavni.php';
 } else {
-    $hobbies = [
-        'trekking',
-        'programování',
-        'školení',
-        'procházky',
-    ];
-
-    $me = [
-        'Jméno' => 'Tomáš',
-        'Příjmení' => 'Fejfar',
-    ];
-
-    $friends = [
-        [
-            'name' => 'Lenka',
-            'surname' => 'Krafková',
-        ],
-        [
-            'name' => 'Tomáš',
-            'surname' => 'Kormaňák',
-        ],
-    ];
-    $template = 'o-mne';
+    require 'controllers/o-mne.php';
 }
 require 'views/layout.php';
