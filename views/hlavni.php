ec3a3f668c776acfdad670f25f68f74d316b6afd
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 21:51:37 +0100

    Z titulku článku odkážeme na detail

--- views/hlavni.php
+++ views/hlavni.php
@@ -1,8 +1,8 @@
 <section>
 <?php foreach ($posts as $post) { ?>
     <article>
-        <h2><?= e($post['title']) ?></h2>
-        <p><?= e($post['text']) ?></p>
+        <a href="/?page=clanek&id=<?= $post['id'] ?>"><h2><?= e($post['title']) ?></h2></a>
+        <p><?= e($post['text']) ?> </p>
     </article>
 <?php } ?>
 </section>
