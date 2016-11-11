ed3bf65f206c2a3ce6c96ec8abb7944355218275
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 21:56:18 +0100

    Vytahneme článek z databáze

--- controllers/clanek.php
+++ controllers/clanek.php
@@ -1,3 +1,7 @@
 <?php
 $template = 'clanek';
 
+$pdo = getDb();
+$statement = $pdo->prepare('SELECT * FROM posts WHERE id = :id');
+$statement->execute(['id' => $_GET['id']]);
+$post = $statement->fetch();
