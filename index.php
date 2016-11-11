54eba2671af32eb4408f0df645cabb0b7ca33b43
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 13:21:05 +0100

    Upravil jsem odsazení, jinak tam žádná změna není

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php
 
 if (empty($_GET['page'])) {
-if (empty($_GET['name'])) {
-    $name = 'World';
+    if (empty($_GET['name'])) {
+        $name = 'World';
+    } else {
+        $name = $_GET['name'];
+    }
+    $greeting = 'Hello ' . $name;
 } else {
-    $name = $_GET['name'];
-}
-$greeting = 'Hello ' . $name;
+    $hobbies = [
+        'trekking',
+        'programování',
+        'školení',
+        'procházky',
+    ];
 
-} else {
-$hobbies = [
-    'trekking',
-    'programování',
-    'školení',
-    'procházky',
-];
+    $me = [
+        'Jméno' => 'Tomáš',
+        'Příjmení' => 'Fejfar',
+    ];
 
-$me = [
-    'Jméno' => 'Tomáš',
-    'Příjmení' => 'Fejfar',
-];
-
-$friends = [
-    [
-        'name' => 'Lenka',
-        'surname' => 'Krafková',
-    ],
-    [
-        'name' => 'Tomáš',
-        'surname' => 'Kormaňák',
-    ],
-];
+    $friends = [
+        [
+            'name' => 'Lenka',
+            'surname' => 'Krafková',
+        ],
+        [
+            'name' => 'Tomáš',
+            'surname' => 'Kormaňák',
+        ],
+    ];
 }
 require 'views/layout.php';
