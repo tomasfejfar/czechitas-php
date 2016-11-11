76d2ef8115c39589872c7a25a16bcc76df6e6301
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 21:41:53 +0100

    Vypíšeme články na hlavní stránku - NEZAPOMENOUT NA ESCAPOVÁNÍ!

--- views/hlavni.php
+++ views/hlavni.php
@@ -1,1 +1,8 @@
-
+<section>
+<?php foreach ($posts as $post) { ?>
+    <article>
+        <h2><?= htmlspecialchars($post['title']) ?></h2>
+        <p><?= htmlspecialchars($post['text']) ?></p>
+    </article>
+<?php } ?>
+</section>
