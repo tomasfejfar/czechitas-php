bdb30594706002b4e68fbb6389a765d374b052f7
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 10:44:02 +0100

    Trošku rozšíříme HTML a přidáme tam běžné tagy HEAD, BODY, atp

--- index.php
+++ index.php
@@ -30,6 +30,16 @@ $friends = [
     ],
 ];
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <h1><?= htmlspecialchars($greeting) ?></h1>
 
 <h2>O mně</h2>
@@ -53,3 +63,5 @@ $friends = [
         <li><?= $hobby ?></li>
     <?php } ?>
 </ul>
+</body>
+</html>
