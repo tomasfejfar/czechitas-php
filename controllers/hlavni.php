7644960dde367ec855efab440001661388681a15
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 21:49:01 +0100

    Uděláme si funkci i na připojení k databázi

--- controllers/hlavni.php
+++ controllers/hlavni.php
@@ -1,12 +1,7 @@
 <?php
 $template = 'hlavni';
 
-$pdoOptions = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-];
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=blog', 'root', '', $pdoOptions);
+$pdo = getDb();
 $statement = $pdo->prepare("SELECT * FROM posts");
 $statement->execute();
 $posts = $statement->fetchAll();
--- functions.php
+++ functions.php
@@ -2,3 +2,12 @@
 function e($what) {
     return htmlspecialchars($what, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 }
+
+function getDb() {
+    $pdoOptions = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    return new PDO('mysql:host=127.0.0.1;dbname=blog', 'root', '', $pdoOptions);
+}
