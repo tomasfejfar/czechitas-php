6d1df22dfec691820ccb1f479d58fc5b7410cffc
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 16:45:46 +0100

    Místo abysme celý skript umřeli, tak ho přesměrujeme

--- controllers/kontakt.php
+++ controllers/kontakt.php
@@ -4,7 +4,7 @@ if (count($_POST)) {
     $name = $_POST['name'];
     $message = $_POST['message'];
     if(mail('tomas.fejfar@gmail.com', 'Zpráva od ' . $name, $message)) {
-        die('Odesláno');
+        header('Location:/?page=podekovani');
     } else {
         die('Email nešlo odeslat :(');
     }
