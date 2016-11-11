6eb1aeeadf956e752a4caad4bd940e1ce07a3694
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 16:44:56 +0100

    Musíme zkontrolovat, že se email opravdu odeslal

--- controllers/kontakt.php
+++ controllers/kontakt.php
@@ -3,7 +3,11 @@ $template = 'kontakt';
 if (count($_POST)) {
     $name = $_POST['name'];
     $message = $_POST['message'];
-    mail('tomas.fejfar@gmail.com', 'Zpráva od ' . $name, $message);
-    die('Odesláno');
+    if(mail('tomas.fejfar@gmail.com', 'Zpráva od ' . $name, $message)) {
+        die('Odesláno');
+    } else {
+        die('Email nešlo odeslat :(');
+    }
+
 }
 
