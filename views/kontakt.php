17831f4efcead6b8fd963c7857817c59ea19a17e
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-11-11 14:07:59 +0100

    Do kontaktu dáme formulář

--- views/kontakt.php
+++ views/kontakt.php
@@ -1,1 +1,7 @@
-Tady je kontakt
+<h1>Kontakt</h1>
+
+<form method="post">
+    <p><label for="name">Jméno:</label><br /><input type="text" name="name"></p>
+    <p><label for="name">Zpráva:</label><br /><textarea name="message"></textarea></p>
+    <p><input type="submit" title="Odeslat" /></p>
+</form>
